2ac78a5315610adcc0bfa1b62cb33f46edb7a5e0
Taiga-td <bensit110620@gmail.com>
2025-09-01 20:35:29 +0700

    ‎

---
+++ assignment.c
@@ -0,0 +1,11 @@
+//Hello All!
+
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Benett");
+    printf("my favorite programing languange is c, phyton, and javascript!");
+
+    return 0;
+}
